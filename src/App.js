3831ca02565139227e48fdd23704e65eb354317b
kadirermaya <kadir.ermaya@gmail.com>
2020-11-18 20:22:05 -0800

    getRecipes function created and EDAMAM API integrated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = "1efc8125";
+  const APP_KEY = "adb1707c25630ea73b5774e260fadf88";
+
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+
+  const getRecipes = async() => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      );
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button
+          className="search-button"
+          type="submit">Search
+          </button>
+      </form>
     </div>
-  );
+  )
 }
+
+
 
 export default App;
